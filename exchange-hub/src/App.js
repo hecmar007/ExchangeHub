8c0c6fd56c072843616902b69583980ed385c9cb
HectorRecloud <hector@recloud.es>
2023-05-13 23:45:25 +0200

    Avançat molt la pagina principal

--- exchange-hub/src/App.js
+++ exchange-hub/src/App.js
@@ -1,15 +1,42 @@
 import './App.css';
 import React from 'react';
 import ResponsiveAppBar from './ResponsiveAppBar';
-import { TextField } from '@mui/material';
+import { TextField, Typography, Grid } from '@mui/material';
 
 
 function App() {
+
+
+  const products = Array.from({ length: 9 }, (_, index) => ({
+    id: index + 1,
+    name: "Placeholder",
+    imageUrl: "https://via.placeholder.com/300",
+  }));
+
+  const handleProductClick = (productId) => {
+    console.log(`Clicked product with ID: ${productId}`);
+    // Add your logic here to handle the click event
+  };
+
+
   return (
     <>
       <ResponsiveAppBar />
-      <div style={{paddingTop: "20px", display: "flex", justifyContent: "center"}}>
-        <TextField id="outlined-basic" label="Search" variant='outlined' />
+      <div style={{ paddingTop: "20px", display: "flex", flexDirection: "column", alignItems: "center" }}>
+        <Typography variant="h3"> ExchangeHub</Typography>
+        <TextField id="outlined-basic" label="Search" variant='outlined' style={{ width: '50%', paddingTop: '15px' }} />
+
+        <Grid container spacing={2} style={{ paddingLeft: '5%', paddingTop: '30px' }}>
+          {products.map((product) => (
+            <Grid item xs={12} sm={6} md={4} key={product.id} >
+              <div onClick={() => handleProductClick(product.id)} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <img src={product.imageUrl} alt="Product" style={{ width: '80%', height: 'auto' }} />
+                <Typography variant='h6' style={{ textAlign: 'center', paddingTop: '10px' }}>{product.name}</Typography>
+              </div>
+            </Grid>
+          ))}
+        </Grid>
+
       </div>
     </>
   );
--- exchange-hub/src/ResponsiveAppBar.js
+++ exchange-hub/src/ResponsiveAppBar.js
@@ -7,36 +7,26 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 
-const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const pages = ['Serveis', 'Productes', 'Els meus anuncis', 'Perfil'];
 
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
   };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
 
   return (
-    <AppBar position="static">
+    <AppBar position="static" style={{background:'black'}}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
@@ -113,7 +103,7 @@ function ResponsiveAppBar() {
           >
             LOGO
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex', justifyContent: ' space-between' } }}>
             {pages.map((page) => (
               <Button
                 key={page}
@@ -123,36 +113,6 @@ function ResponsiveAppBar() {
                 {page}
               </Button>
             ))}
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
           </Box>
         </Toolbar>
       </Container>
